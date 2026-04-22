c69cdd97a33cbcca0eaf309e1ffc76a8af192dd6
hari karthick <harikarthickr20@gmail.com>
2026-04-22 07:36:23 +0530

    Added UC1: ability to add a new contact to address book

--- AddressBook.java
+++ AddressBook.java
@@ -1,3 +1,45 @@
+
+
+import java.util.ArrayList;
+
 public class AddressBook {
-    
+    private ArrayList<ContactPerson> contacts;
+
+    public AddressBook() {
+        contacts = new ArrayList<>();
+    }
+
+    public void addContact(ContactPerson person) {
+        contacts.add(person);
+        System.out.println("Contact added successfully.");
+    }
+
+    public void displayAllContacts() {
+        if (contacts.isEmpty()) {
+            System.out.println("No contacts found.");
+            return;
+        }
+
+        for (ContactPerson person : contacts) {
+            person.displayContact();
+        }
+    }
+
+    public ContactPerson findContactByFirstName(String firstName) {
+        for (ContactPerson person : contacts) {
+            if (person.getFirstName().equalsIgnoreCase(firstName)) {
+                return person;
+            }
+        }
+        return null;
+    }
+
+    public boolean deleteContactByFirstName(String firstName) {
+        ContactPerson person = findContactByFirstName(firstName);
+        if (person != null) {
+            contacts.remove(person);
+            return true;
+        }
+        return false;
+    }
 }
--- AddressBookMain.java
+++ AddressBookMain.java
@@ -1,7 +1,43 @@
 
+import java.util.Scanner;
 
 public class AddressBookMain {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        AddressBook addressBook = new AddressBook();
+
         System.out.println("Welcome to Address Book Program");
+
+        System.out.print("Enter First Name: ");
+        String firstName = scanner.nextLine();
+
+        System.out.print("Enter Last Name: ");
+        String lastName = scanner.nextLine();
+
+        System.out.print("Enter Address: ");
+        String address = scanner.nextLine();
+
+        System.out.print("Enter City: ");
+        String city = scanner.nextLine();
+
+        System.out.print("Enter State: ");
+        String state = scanner.nextLine();
+
+        System.out.print("Enter Zip: ");
+        String zip = scanner.nextLine();
+
+        System.out.print("Enter Phone Number: ");
+        String phoneNumber = scanner.nextLine();
+
+        System.out.print("Enter Email: ");
+        String email = scanner.nextLine();
+
+        ContactPerson person = new ContactPerson(firstName, lastName, address, city, state, zip, phoneNumber, email);
+        addressBook.addContact(person);
+
+        System.out.println("\nContact Details:");
+        addressBook.displayAllContacts();
+
+        scanner.close();
     }
 }
