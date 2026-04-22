6fdda94a2b69a2fb3371c389f73b8145b384ef6c
hari karthick <harikarthickr20@gmail.com>
2026-04-22 08:37:49 +0530

    Added UC6: ability to add multiple address books with unique names

--- AddressBookMain.java
+++ AddressBookMain.java
@@ -1,54 +1,77 @@
 
 
-
+import java.util.HashMap;
+import java.util.Map;
 import java.util.Scanner;
 
 public class AddressBookMain {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        AddressBook addressBook = new AddressBook();
+        Map<String, AddressBook> addressBookMap = new HashMap<>();
 
         System.out.println("Welcome to Address Book Program");
 
-        String choice;
+        String addNewBookChoice;
 
         do {
-            System.out.println("\nEnter contact details:");
+            System.out.print("\nEnter Address Book Name: ");
+            String bookName = scanner.nextLine();
 
-            System.out.print("Enter First Name: ");
-            String firstName = scanner.nextLine();
+            if (addressBookMap.containsKey(bookName)) {
+                System.out.println("Address Book with this name already exists.");
+            } else {
+                AddressBook addressBook = new AddressBook();
+                addressBookMap.put(bookName, addressBook);
+                System.out.println("Address Book '" + bookName + "' created successfully.");
 
-            System.out.print("Enter Last Name: ");
-            String lastName = scanner.nextLine();
+                String addContactChoice;
+                do {
+                    System.out.println("\nEnter contact details for Address Book: " + bookName);
 
-            System.out.print("Enter Address: ");
-            String address = scanner.nextLine();
+                    System.out.print("Enter First Name: ");
+                    String firstName = scanner.nextLine();
 
-            System.out.print("Enter City: ");
-            String city = scanner.nextLine();
+                    System.out.print("Enter Last Name: ");
+                    String lastName = scanner.nextLine();
 
-            System.out.print("Enter State: ");
-            String state = scanner.nextLine();
+                    System.out.print("Enter Address: ");
+                    String address = scanner.nextLine();
 
-            System.out.print("Enter Zip: ");
-            String zip = scanner.nextLine();
+                    System.out.print("Enter City: ");
+                    String city = scanner.nextLine();
 
-            System.out.print("Enter Phone Number: ");
-            String phoneNumber = scanner.nextLine();
+                    System.out.print("Enter State: ");
+                    String state = scanner.nextLine();
 
-            System.out.print("Enter Email: ");
-            String email = scanner.nextLine();
+                    System.out.print("Enter Zip: ");
+                    String zip = scanner.nextLine();
 
-            ContactPerson person = new ContactPerson(firstName, lastName, address, city, state, zip, phoneNumber, email);
-            addressBook.addContact(person);
+                    System.out.print("Enter Phone Number: ");
+                    String phoneNumber = scanner.nextLine();
 
-            System.out.print("Do you want to add another contact? (yes/no): ");
-            choice = scanner.nextLine();
+                    System.out.print("Enter Email: ");
+                    String email = scanner.nextLine();
 
-        } while (choice.equalsIgnoreCase("yes"));
+                    ContactPerson person = new ContactPerson(firstName, lastName, address, city, state, zip, phoneNumber, email);
+                    addressBook.addContact(person);
 
-        System.out.println("\nAll Contacts in Address Book:");
-        addressBook.displayAllContacts();
+                    System.out.print("Do you want to add another contact to this Address Book? (yes/no): ");
+                    addContactChoice = scanner.nextLine();
+
+                } while (addContactChoice.equalsIgnoreCase("yes"));
+            }
+
+            System.out.print("\nDo you want to add another Address Book? (yes/no): ");
+            addNewBookChoice = scanner.nextLine();
+
+        } while (addNewBookChoice.equalsIgnoreCase("yes"));
+
+        System.out.println("\nDisplaying All Address Books:");
+
+        for (Map.Entry<String, AddressBook> entry : addressBookMap.entrySet()) {
+            System.out.println("\nAddress Book Name: " + entry.getKey());
+            entry.getValue().displayAllContacts();
+        }
 
         scanner.close();
     }
